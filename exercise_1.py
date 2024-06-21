d8a480e461ff4dc30c6222731b3030bb4a338309
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2024-06-21 16:16:32 +0100

    Create exercise_1.py

---
+++ exercise_1.py
@@ -0,0 +1,6 @@
+file_name = "demo_1"
+
+def stt_testing(file):
+    print("The transcript from \'" + file_name + "\' is: " + speech_to_text(str(os.getcwd() + "/sample/" + file + ".wav")))
+
+stt_testing(file_name)
